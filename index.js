63b732ed490d561e6521c990a590800357674bb8
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-04-01 19:39:55 -0400

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let returnValue = "";
+
+  if (someValue === 400) {
+    // If I use <= it is not working but somehow it is working with ===
+    returnValue = "Free sample";
+  } else if (someValue >= 400 && someValue <= 2000) {
+    returnValue = "That will be twenty bucks.";
+  } else if (someValue > 2000 && someValue < 2500) {
+    returnValue = "I will gladly take your thirty bucks."
+  } else if (someValue > 2500) {
+    returnValue = "No can do.";
+  } else {
+    returnValue = "This one is on me!";
+  }
+  return returnValue;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  return (cityName === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipType){
   // Write your code here!
+  let returnValue = "";
+
+  switch (tipType) {
+    case "generous":
+      returnValue = "Thank you so much.";
+      break;
+    case "not as generous":
+      returnValue = "Thank you.";
+      break;
+    default:
+      returnValue = "Bye.";
+      break;
+  }
+
+  return returnValue;
 }
